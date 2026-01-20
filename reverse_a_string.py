4bff1657754d41098d767ddb15b4a40a04eaf8f7
tejaskopperla-code <tejas.kopperla@gmail.com>
2026-01-20 19:29:09 +0530

    Add files via upload

---
+++ reverse_a_string.py
@@ -0,0 +1,9 @@
+string  = input('pleade enter your own string')
+
+string2 = ('')
+
+for i in string:
+    string2 = i + string2
+
+    print("\n the original string =", string)
+    print("the reversed string =", string2)
